555dd9ae6f5d9dc9dff2a3b26f9f302bcbff7c42
Winston <winston@Winstons-MacBook-Pro.local>
2019-04-02 18:21:31 -0700

    whew, can finally not have to worry about the full send  --nice

--- login_info.py
+++ login_info.py
@@ -1,6 +1,6 @@
 '''account info'''
 
-# email or handle
-user = ''  
-# password
-pwrd = ''  
+# email or handle (if = '__OPT-OUT__' , will be requested upon execution)
+user = '__OPT-OUT__'  
+# password (if = '__OPT-OUT__' , will be requested upon execution)
+pwrd = '__OPT-OUT__'  
