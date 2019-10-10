c105049bf018c27f754f35c0c776d6c666255454
gumdropsteve <warobson@gmail.com>
2019-10-10 07:19:29 -0700

    cleaned up and eliminated a lot of repetitive tasks; unable to actually tweet, yet; getting a lot of 'not reachable by keyboard' from selenium (hince mess and try/except) -- working on this more later, hopefully don't have to resort to API

--- tweet.py
+++ tweet.py
@@ -4,140 +4,143 @@ from selenium.webdriver.common.keys import Keys
 from paths import login_page, user_box, pass_box, login_button, tweet_box, tweet_button
 from login_info import user, pwrd
 
-# GeckoDriver setup
-options = webdriver.FirefoxOptions()  
-options.set_preference( 'dom.push.enabled' , False )  # blocks popups 
 
+class TwitterBot:    
 
-class Tweet:    
-    '''
-    simple overview:
-        1) open browser
-        2) log in to twitter
-        3) locate and engage tweet box
-        4) send tweet (keys) to tweet box
-        5) tweet
-    note: 
-        sleep included for loadtimes
-    '''
-
-    def __init__( self , username , password , status ):
+    def __init__(self, username):
         '''
-        initialized)
-            1) self
-                >> self
-            2) username
+        inputs:
+            > username
                 >> email, handle, phone number, etc... of account tweet will be sent from
-            3) password
-                >> password of account tweet will be sent from 
-            4) statua
-                >> the tweet being sent 
         '''
+        # who are we working with? (store user)
         self.username = username
-        self.password = password
-        self.status = status
 
-    # done here to avoid browser popping up before status is set
-    def set_browser( self ):
+    def set_browser(self, block_popups=True):
         '''
-        set driver (Firefox GeckoDriver) using previously defined options
+        function: set driver (Firefox GeckoDriver) using previously defined options
+        inputs:
+            > block_popups
+                >> do you want pop ups blocked? (defaults to True)
         '''
-        self.driver = webdriver.Firefox( options=options ) 
+        # are we blocking popups? (default answer is 'yes')
+        if block_popups == True:
+            # GeckoDriver setup
+            options = webdriver.FirefoxOptions()
+            # block popups 
+            options.set_preference('dom.push.enabled', False)  
+            # set webdriver with options
+            self.driver = webdriver.Firefox(options=options) 
+        # we are not blocking popups 
+        else:
+            # set default webdriver
+            self.driver = webdriver.Firefox() 
     
-    def login( self ):
+    def login(self, password):
         '''
-        load Twitter login page and log in to Twitter
+        function: load Twitter login page and log in to Twitter
+        inputs:
+            > password
+                >> password of account tweet will be sent from 
         '''
         # load login page
-        self.driver.get( login_page ) 
-        sleep( 1 )
+        self.driver.get(login_page) 
+        sleep(1)
         # send username to username input box
-        self.driver.find_element_by_css_selector( user_box ).send_keys( self.username )
-        sleep( 1 )
+        self.driver.find_element_by_css_selector(user_box).send_keys(self.username)
+        sleep(1)
         # send password to password input box
-        self.driver.find_element_by_css_selector( pass_box ).send_keys( self.password )
-        sleep( 1 )
+        self.driver.find_element_by_css_selector(pass_box).send_keys(password)
+        sleep(1)
         # click button to login
-        self.driver.find_element_by_css_selector( login_button ).click()
-        sleep( 2 )
+        self.driver.find_element_by_css_selector(login_button).click()
+        sleep(5)
     
-    def send_status( self ):
+    def send_status(self, status):
         '''
-        find tweet box on Twitter home page and send it the status input
+        function: find tweet box on Twitter home page and send it the status input
+        inputs:
+            > status
+                >> the tweet being sent 
         '''
+        # store status
+        self.status = status
         # send status to status input box 
-        self.driver.find_element_by_css_selector( tweet_box ).send_keys( self.status )
-        sleep( 1 )
+        sleep(3)
+        tweet_space = '/html/body/div/div/div/div/main/div/div/div/div[1]/div/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div'
+        try:
+            self.driver.find_elements_by_xpath(tweet_space)[0].send_keys(self.status)
+            print('SUCCESS')
+            sleep(2)
+        except:
+            print('EXCEPT')
+            sleep(2)
+            self.driver.find_element_by_css_selector(tweet_box).click()
+            sleep(2)
+            self.driver.find_elements_by_xpath(tweet_space)[0].send_keys(self.status)
+        sleep(2)
         # click button to tweet
-        self.driver.find_element_by_css_selector( tweet_button ).click()
-        sleep( 5 )
+        self.driver.find_element_by_css_selector(tweet_button).click()
+        sleep(5)
     
-    def close_browser( self ):
+    def close_browser(self):
         '''
-        closes browser being used  
+        quits browser being used  
         '''
-        self.driver.close()
-    
-    # do it all 
-    def run_all( self ):
-        '''
-        combination of all functions with output confirmation 
-        '''
-        # set GeckoDriver
-        self.set_browser()
-        # log in to Twitter
-        self.login()
-        # tweet out status input
-        self.send_status()
-        # close browser
-        self.close_browser()
-        # report back the tweet that was sent and account it was sent to
-        return f'sent tweet: { self.status } \nto account: { self.username }'
+        self.driver.quit()
 
 
-# let's do it
+# default funcitonality = log in and tweet
 if __name__ == '__main__':
-    # check that username and password are logical
-    if len( user ) < 1 or len( pwrd ) < 1:
+
+    # is username blank?
+    if user.strip() == '':
+        # option to enter username (eliminate error spaces w/ strip)
+        user = input('username: ').strip()
+    else:
+        # eliminate error spaces just in case
+        user = user.strip() 
+
+    # is password blank?
+    if pwrd.strip() == '':
+        # option to enter password (eliminate error spaces w/ strip)
+        pwrd = input('password: ').strip()
+    else:
+        # eliminate error spaces just in case
+        pwrd = pwrd.strip() 
+
+    # usernames must be less than 15 characters and passwords must be at least 6 characters
+    if (len(user) > 15) or (len(pwrd) < 6):
         # username or password deemed invalid
         raise Exception('illogical username or password\nplease check you have correctly entered your login information \n@ login_info.py\n'
                         f'current user = {user} (len={len(user)})\ncurrent password = {pwrd} (len={len(pwrd)})') 
 
     # call for status
-    my_tweet = input( "what's your status? " )
-
-    # check that username and password are logical length
-    if len( user ) < 1 or len( pwrd ) < 1:
-        # username or password deemed invalid due to lenght of less than 1 character
-        raise Exception('illogical username or password\nplease check you have correctly entered your login information \n@ login_info.py\n'
-                        f'current user = {user} (len={len(user)})\ncurrent password = {pwrd} (len={len(pwrd)})') 
+    my_tweet = 'testing out the new bot.. this thing on?'  # input("what's your status? ")
 
     # check max length for compliance
-    if len( my_tweet ) > 280:
+    if len(my_tweet) > 280:
         # tweet deemed too long
-        raise Exception( f'tweet too long \n{ len( my_tweet ) } > 280' )
+        raise Exception(f'tweet too long: {len(my_tweet)} > 280\n')
     # check min length for compliance
-    if len( my_tweet ) < 1:
+    elif len(my_tweet) < 1:
         # tweet deemed too short 
-        raise Exception( f'tweet too short \n{ len( my_tweet ) } < 1' )
+        raise Exception(f'tweet too short: {len(my_tweet)} < 1\n')
 
-    # option to enter username upon call
-    if user == '__OPT-OUT__':
-        user = input('username: ')
+    # set TwitterBot instance
+    tb = TwitterBot(username=user)
 
-    # check that username length is logical 
-    if len( user ) < 1:
-        # username deemed invalid due to lenght of less than 1 character
-        raise Exception(f'illogical username or password\ncurrent user = {user} (len={len(user)})\ncurrent password = {pwrd} (len={len(pwrd)})') 
+    # start up selenium webdriver 
+    tb.set_browser()
 
-    # option to enter password upon call
-    if pwrd == '__OPT-OUT__':
-        pwrd = input('password: ')
+    # log in to Twitter
+    tb.login(password=pwrd)
 
-    # check that password is logical length
-    if len( pwrd ) < 1:
-        # password deemed invalid due to lenght of less than 1 character
-        raise Exception(f'illogical username or password\ncurrent user = {user} (len={len(user)})\ncurrent password = {pwrd} (len={len(pwrd)})') 
+    # tweet out status input
+    tb.send_status(status=my_tweet)
 
-    # display out in terminal 
-    print( Tweet( user , pwrd , my_tweet ).run_all() )
+    # shut down shop
+    tb.close_browser()
+
+    # report back the tweet that was sent and account it was sent to
+    print(f'sent tweet: {tb.status} \nto account: {tb.username}')
