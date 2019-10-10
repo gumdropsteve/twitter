d840ffff5e5f1bf941a149bbd39704701c4c2122
gumdropsteve <warobson@gmail.com>
2019-10-10 07:18:01 -0700

    no longer need opt out, now a simple blank string is fine

--- login_info.py
+++ login_info.py
@@ -1,6 +1,6 @@
-'''account info'''
-
-# email or handle (if = '__OPT-OUT__' , will be requested upon execution)
-user = '__OPT-OUT__'  
-# password (if = '__OPT-OUT__' , will be requested upon execution)
-pwrd = '__OPT-OUT__'  
+'''account info
+'''
+# email or handle (if empty, will be requested upon execution)
+user = 'winstonrobson_'  
+# password (if empty, will be requested upon execution)
+pwrd = ''  
